1f624f83d5397414c10e4316e05d5a944a8075c4
Laiyi Fu <fulaiyi321@163.com>
2021-05-18 14:14:36 +0800

    Update Network.py

--- Network.py
+++ Network.py
@@ -37,23 +37,6 @@ class up_conv(nn.Module):
         return x
 
 
-
-
-class single_conv(nn.Module):
-    def __init__(self,ch_in,ch_out):
-        super(single_conv,self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(ch_in, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self,x):
-        x = self.conv(x)
-        return x
-
-
-
 class U_Net(nn.Module):
     def __init__(self,img_ch=3,output_ch=1):
         super(U_Net,self).__init__()
